eaa9b8bb30e5457c6762de3e6aa87738641b94bd
GANESH SADANALA <40536512+ganesh-sadanala@users.noreply.github.com>
2024-03-30 19:10:23 -0400

    Create InvertedIndex.java

---
+++ Amazon-OnlineShoppingSystem/ClassDiagram/src/Utils/InvertedIndex.java
@@ -0,0 +1,36 @@
+class InvertedIndex{
+  Map<String, List<Integer>> nameIndex;
+  Map<String, List<Integer>> categoryIndex;
+  Map<String, List<Integer>> priceRangeIndex;
+
+  public InvertedIndex(){
+    nameIndex = new HashMap<>();
+    categoryIndex = new HashMap<>();
+    priceRangeIndex = new HashMap<>();
+  }
+
+  public void buildIndex(List<Product> products){
+    for(Product product: products){
+      addToIndex(nameIndex, product.name, product.id);
+      addToIndex(categoryIndex, product.category, product.id);
+      addToIndex(priceRangeIndex, product.priceRange, product.id)
+    }
+  }
+
+  private void addToIndex(Map<String, List<Integer>> index, String key, int productId) {
+      index.putIfAbsent(key, new ArrayList<>());
+      index.get(key).add(productId);
+  }
+
+  public List<Integer> searchByName(String name) {
+        return nameIndex.getOrDefault(name, Collections.emptyList());
+    }
+
+    public List<Integer> searchByCategory(String category) {
+        return categoryIndex.getOrDefault(category, Collections.emptyList());
+    }
+
+    public List<Integer> searchByPriceRange(String priceRange) {
+        return priceRangeIndex.getOrDefault(priceRange, Collections.emptyList());
+    }
+}
